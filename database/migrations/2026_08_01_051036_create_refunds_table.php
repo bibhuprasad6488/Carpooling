2030bd3c08d0fbe85da713b16a8bbcfd687e9242
Bibhu Prasad <119917682+bibhuprasad6488@users.noreply.github.com>
2026-08-01 10:53:40 +0530

    added refund table and amount column to payments

---
+++ database/migrations/2026_08_01_051036_create_refunds_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('refunds', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('booking_id');
+            $table->string('refund_id')->nullable();
+            $table->decimal('refund_amount', 10, 2);
+            $table->text('reason_of_refund')->nullable();
+            $table->string('status')->default('requested');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('refunds');
+    }
+};
