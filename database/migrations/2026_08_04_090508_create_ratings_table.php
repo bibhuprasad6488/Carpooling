e1a8a52e2d856decdf0396a724538c29358ff5e6
Bibhu Prasad <119917682+bibhuprasad6488@users.noreply.github.com>
2026-08-06 12:40:53 +0530

    Rating Added

---
+++ database/migrations/2026_08_04_090508_create_ratings_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('ratings', function (Blueprint $table) {
+            $table->id();
+            $table->integer('ride_id');
+            $table->integer('booking_id');
+            $table->integer('passenger_id');
+            $table->unsignedTinyInteger('rating');
+            $table->text('review')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('ratings');
+    }
+};
